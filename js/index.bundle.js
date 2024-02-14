8313675c449b8da356b7f3834ef866b0aeb63083
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-02-14 20:19:22 +0300

    JS owl carousel slider btns

--- js/index.bundle.js
+++ js/index.bundle.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded",(function(){$(document).ready((function(){$("#slider-blog").owlCarousel({items:2,loop:!0})}))}));
+$(document).ready(function(){
+
+    // Находим блок с слайдерами
+    const owl = $('#slider-blog');
+    // Запускаем карусель
+    owl.owlCarousel({
+        items: 2,
+        loop: true
+    });
+
+    // Находим кастомные кнопки вперед/назад
+    const prev = $('#sliderPrev');
+    const next = $('#sliderNext');
+
+    // Клик на кнопку назад и прокрутка карусели
+    prev.click(function(){
+        owl.trigger('prev.owl.carousel')
+    });
+
+    // Клик на кнопку вперед и прокрутка карусели
+    next.click(function(){
+        owl.trigger('next.owl.carousel')
+    });
+  });
